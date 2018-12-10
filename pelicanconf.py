6595130436fa325de80c34e8be6d166eaeb76a7b
XonqNopp <xonqnoppp@gmail.com>
2018-12-10 15:30:37 +0100

    switching to pelican (easier to do blog-like)

---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'XonqNopp'
+SITENAME = 'I rant therefore I am'
+SITEURL = ''
+GITHUB_URL = 'https://github.com/XonqNopp/XonqNopp.github.io'
+
+THEME = 'notmyidea'
+#THEME = 'pelican-simplegrey'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = [
+    #('Pelican', 'http://getpelican.com/'),
+    #('Python.org', 'http://python.org/'),
+]
+
+# Social widget
+SOCIAL = [
+    #('You can add links in your config file', '#'),
+    #('Another social link', '#'),
+]
+
+DEFAULT_PAGINATION = 42
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
